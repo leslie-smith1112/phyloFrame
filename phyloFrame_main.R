aea6f937cc32cae6ac1bcb7b6b8ff591f26c1cb3
leslie-smith1112 <leslie.smith1@ufl.edu>
2023-06-19 17:40:30 -0400

    cleaned up comments and removed functions not used

--- phyloFrame_main.R
+++ phyloFrame_main.R
@@ -9,15 +9,13 @@ library(workflows) #put model in workflow
 library(rpart.plot)
 library(vip)
 library(igraph)
-#phyloFrame(network, exomeAF, dz.signature, consensus_genes, expression, samples, clinical, out, "breast")
 source("/home/leslie.smith1/blue_kgraim/leslie.smith1/Repositories/phyloFrame/new_exomeAF_nosex_ordering.R")
 source("/home/leslie.smith1/blue_kgraim/leslie.smith1/Repositories/phyloFrame/expression_elasticnet.R")
-source("/home/leslie.smith1/blue_kgraim/leslie.smith1/Repositories/phyloFrame/diseases/breast.R")
 
 
 get.network <- function(tissue.network, dz.sig, neighbor, edge.weight){
-  neighbor <- 1
-  cut.graph <- tissue.network[((tissue.network$Connection >= 0.2) & (tissue.network$Connection < 0.51)),]
+  neighbor <- 2
+  cut.graph <- tissue.network[((tissue.network$Connection >= 0.2) & (tissue.network$Connection < 0.51)),] 
   new.sig <- dz.sig[(dz.sig %in% cut.graph$Gene1 | dz.sig %in% cut.graph$Gene2)]
   ## -- some of the dz genes are not in network -> trim them out -- ##
   dat.graph <- graph.data.frame(cut.graph, directed =  FALSE)
@@ -33,68 +31,21 @@ get.network <- function(tissue.network, dz.sig, neighbor, edge.weight){
   print("Completed finding gene signature neighborhood, moving to Exome allele frequencies.")
   return(node.names)
 }
-get.genes.V1 <- function(tiss.net, dz.genes, the.node, the.edge, expression, exomeAF){
-  network.genes <- get.network(tiss.net, dz.genes, the.node, the.edge)
-  top.anc.dat <- order_frequencies(network.genes, exomeAF, expression)
-  temp.anc <- unlist(top.anc.dat)
-  ##only keep top n most variable acnestry genes 
-  #temp.anc <- top.anc.dat[!(top.anc.dat %in% dz.genes)]
-  temp.anc <- temp.anc[temp.anc %in% colnames(expression)] # make sure genes are in the expression matrix
-  num.genes <- length(temp.anc)
-  top.anc.dat <- unique(c(dz.genes, temp.anc))
-  top.anc.dat <- c(top.anc.dat, "subtype")
-  
-  expr.variance <- apply(expression, 2, var)
-  var.ordered <- expr.variance[order(expr.variance, decreasing = TRUE)]
-  genes.keep <- var.ordered[1:num.genes]
-  n.variable.genes<- names(genes.keep)
-  n.variable.genes <- c(dz.genes, n.variable.genes, "subtype")
-  genes <- list("phyloFrame" = top.anc.dat, "benchmark" = n.variable.genes)
-  return(genes)
- 
-}
 
 get.genes.V2 <- function(tiss.net, dz.genes, the.node, the.edge, expression, exomeAF){
   network.genes <- get.network(tiss.net, dz.genes, the.node, the.edge)
   top.anc.dat <- order_frequencies(network.genes, exomeAF, expression) # base genes will be model genes (the base sig)
   top.anc.dat <- unlist(top.anc.dat)
-  # print(length(top.anc.dat))
-  # if(length(top.anc.dat) == 0){
-  #   top.anc.dat = pf_top_varying_genes(expression, 2000)
-  # }
   # make sure genes are in the expression matrix
   top.anc.dat <- top.anc.dat[top.anc.dat %in% colnames(expression)] # make sure genes are in expression matrix
   top.anc.dat <- top.anc.dat[!(top.anc.dat %in% dz.genes)] 
-  # do not increase expression of initial base genes
   print(length(top.anc.dat))
   genes <- list("phyloFrame" = top.anc.dat, "benchmark" = NULL) 
   return(genes)
 }
 
-ancestry.rescale<- function(expr.dat, ancestry.list){
-  #do.call()
-  #gene list of variable vs equitable do ttest two groups we are prediciting to see correlation  - double check we are actualyl gertting better expression. 
-  for (i in 1:length(ancestry.list)) {
-    gene.vector <- expr.dat[,ancestry.list[i]]
-    cur.max <- max(gene.vector)
-    cur.min <- min(gene.vector)
-    new.max <- cur.max * 700
-    rescaled.vector <- rescale(gene.vector, to = c(cur.min, new.max), from = range(gene.vector, na.rm = TRUE, finite = TRUE))
-    expr.dat[,ancestry.list[i]] <- rescaled.vector
-  }
-  # index <- which(expr.dat== max(expr.dat), arr.ind = TRUE)
-  # max.row <- index[1,1]
-  # max.col <- index[1,2]
-  # cur.max <- expr.dat[max.row, max.col]
-  # new.max <- 2*cur.max
-  # temp.expr <- expr.dat[,ancestry.genes]
-  # lapply(temp[,ancestry.genes], gene.rescale)
-  return(expr.dat)
-}
-
-## phyloFrame 2.0 added to accomodate for selected genes being null for inital elastic net phyloframe run 
+## phyloFrame call to run elastic net on the given genes with correct penalty
 phyloFrame <- function(selected.genes, expression.mat, directory, out.file, mixture){
-  
   if(is.null(selected.genes) == FALSE){
     expr <- expression.mat[,colnames(expression.mat) %in% selected.genes]
   }else{
